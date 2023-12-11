096e02a6f350f7dbc6399f5c09973f791ba14c6d
alpha <2735903433@qq.com>
2023-12-11 10:14:53 +0800

    fix pentype

---
+++ MangoWidgets.Core/PenType.cs
@@ -0,0 +1,14 @@
+﻿namespace MangoWidgets.Core;
+
+public enum PenType
+{
+    /// <summary>
+    /// 普通笔
+    /// </summary>
+    Normal,
+    
+/*    /// <summary>
+    /// 荧光笔
+    /// </summary>
+    Highlighter,*/
+}
